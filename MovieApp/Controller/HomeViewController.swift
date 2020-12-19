5eaad3f45b7e810dc2c50d9a58b0221eaf97cb9b
Udoy Soumik <udoy.soumik@gmail.com>
2020-12-19 13:48:42 +0600

    Trending UI done

--- MovieApp/Controller/HomeViewController.swift
+++ MovieApp/Controller/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -55,9 +56,33 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         case 1:
             break
         case 2:
-            cell?.movieNameLabel.text = trendingContentArr[indexPath.row]["title"] as? String
+            
+            if let name = trendingContentArr[indexPath.row]["title"] as? String {
+                cell?.movieNameLabel.text = name
+            }else if let name = trendingContentArr[indexPath.row]["name"] as? String{
+                cell?.movieNameLabel.text = name
+            }
+            else{
+                cell?.movieNameLabel.text = "Not Available"
+            }
+            
             cell?.movieRating.text = "\(trendingContentArr[indexPath.row]["vote_average"] as? Double ?? 0.0)"
-            cell?.releaseDate.text = trendingContentArr[indexPath.row]["release_date"] as? String
+            if let releaseDate = trendingContentArr[indexPath.row]["release_date"] as? String {
+                cell?.releaseDate.text = releaseDate
+            }else if let airDate = trendingContentArr[indexPath.row]["first_air_date"] as? String{
+                cell?.releaseDate.text = airDate
+            }
+            else{
+                cell?.releaseDate.text = "Coming Soon"
+            }
+            
+            if let imageURL = trendingContentArr[indexPath.row]["poster_path"] as? String{
+                let imageURL = URL(string: "https://image.tmdb.org/t/p/w342" + imageURL)
+                cell?.moviePosterImage.sd_setImage(with: imageURL, placeholderImage: UIImage(), options: .progressiveLoad)
+            }
+            else{
+                cell?.moviePosterImage.image = UIImage()
+            }
             
             break
         default:
@@ -77,7 +102,10 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
             return "Trending Content"
         }
     }
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
 
 }
 
